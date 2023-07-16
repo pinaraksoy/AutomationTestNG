2ec3e38592f81baffd4f2119b0d3d565f627863b
pinaraksoy <pinaraksoy_22@hotmail.com>
2023-07-16 13:59:43 +0300

    TC 9 added

---
+++ src/test/java/comAutomationTesting/Tests/Homepage/TC9_Arrivals_AddToBasketItemsCoupon.java
@@ -0,0 +1,104 @@
+package comAutomationTesting.Tests.Homepage;
+
+import comAutomationTesting.Pages.Homepage;
+import comAutomationTesting.utilities.ConfigurationReader;
+import comAutomationTesting.utilities.Driver;
+import comAutomationTesting.utilities.ReusableMethods;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TC9_Arrivals_AddToBasketItemsCoupon {
+    Homepage homepage = new Homepage();
+    JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+    String imageText;
+    @BeforeClass
+    public void setUp() {
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
+    }
+
+    //3) Click on Shop Menu
+    @Test(priority = 1)
+    public void ClickOnShopMenu() {
+        ReusableMethods.clickWithJS(homepage.shopButton);
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(homepage.shopButton, 2);
+    }
+
+    //4) Now click on Home menu button
+    @Test(priority = 2)
+    public void clickOnHomeMenuButton() {
+        homepage.homeButton.click();
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(homepage.homeButton, 3);
+    }
+
+    @Test(priority = 3)
+    public void ThreeArrivalOnly() {
+
+        //5) Test whether the Home page has Three Arrivals only
+        //6) The Home page must contain only three Arrivals
+        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+        jsexecutor.executeScript("window.scrollBy(0,1000)");
+        int actualArrivals = homepage.threeArrivalOnly.size();
+        int expectedArrivals = 3;
+        Assert.assertEquals(actualArrivals, expectedArrivals);
+
+    }
+    @Test(priority = 4)
+    public void imagesInArrivals() {
+        //7) Now click the image in the Arrivals
+        imageText = homepage.itemInImage1.getText().toLowerCase();
+        homepage.image1.click();
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(homepage.image1, 5);
+
+
+        //8) Test whether it is navigating to next page where the user can add that book into his basket.
+        //9) Image should be clickable and should navigate to next page where user can add that book to his basket
+
+        Assert.assertTrue(Driver.getDriver().getTitle().toLowerCase().contains(imageText));
+        assert homepage.addToBasketButton.isDisplayed();
+    }
+
+    @Test(priority = 5)
+    public void Arrivals_AddToBasketItems() {
+        //10) Click on the Add To Basket button which adds that book to your basket
+        homepage.addToBasketButton.click();
+        String expectedAddedMessage = "“Selenium Ruby” has been added to your basket.";
+        String actualAddedMessage = homepage.addedToBasketMessage.getText();
+        System.out.println("actualAddedMessage = " + actualAddedMessage);
+        Assert.assertTrue(actualAddedMessage.contains(expectedAddedMessage));
+
+        //11) User can view that Book in the Menu item with price.
+        homepage.viewBasketButton.click();
+        Assert.assertEquals(homepage.productName.getText().toLowerCase(),imageText);
+        Assert.assertTrue(homepage.productPrice.isDisplayed());
+
+        //12) Now click on Item link which navigates to proceed to check out page.
+        homepage.itemLink.click();
+        //13) User can click on the Item link in menu item after adding the book in to the basket which leads to the check out page
+        jsexecutor.executeScript("window.scrollBy(0,750)");
+        homepage.proceedToCheckout.click();
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Checkout"));
+        homepage.itemLink.click();
+    }
+
+    @Test(priority = 6)
+    public void Arrivals_AddToBasketItemsCoupon() {
+        //14) Enter the Coupon code as ‘krishnasakinala’ to get 50rps off on the total.
+         homepage.inputCouponCode.sendKeys(ConfigurationReader.getProperty("coupon.code"));
+
+        //15) User can able to apply coupon by entering ‘krishnasakinala’ in the coupon textbox which give 50rps off on the total price
+         homepage.applyCouponButton.click();
+         Assert.assertEquals(homepage.addedToBasketMessage.getText(),"Coupon code applied successfully.");
+         jsexecutor.executeScript("window.scrollBy(0,750)");
+         System.out.println(homepage.freeCouponAmount.getText());
+         Assert.assertTrue(homepage.freeCouponAmount.getText().equals("₹50.00"));
+
+    }
+}
