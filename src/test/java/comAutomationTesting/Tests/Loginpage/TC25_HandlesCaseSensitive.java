1ed7c6b1eb9d003a057dd08141af6fd884435186
pinaraksoy <pinaraksoy_22@hotmail.com>
2023-07-25 17:12:04 +0300

    TC25 added

---
+++ src/test/java/comAutomationTesting/Tests/Loginpage/TC25_HandlesCaseSensitive.java
@@ -0,0 +1,38 @@
+package comAutomationTesting.Tests.Loginpage;
+
+import comAutomationTesting.Pages.LoginPage;
+import comAutomationTesting.utilities.ConfigurationReader;
+import comAutomationTesting.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC25_HandlesCaseSensitive {
+    //25. Login-Handles case sensitive
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void handlesCaseSensitive() {
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
+        //3) Click on My Account Menu
+        loginPage.myAccountButton.click();
+        Driver.getDriver().navigate().refresh();
+        loginPage.myAccountButton.click();
+
+        //4) Enter the case changed username in username textbox
+        loginPage.inputUsername.sendKeys(ConfigurationReader.getProperty("valid.email").toUpperCase());
+
+        //5) Enter the case chenged password in the password tetxbox
+        loginPage.inputPassword.sendKeys(ConfigurationReader.getProperty("valid.password").toUpperCase());
+
+        //6) Now click on login button
+        loginPage.loginButton.click();
+
+        //7) Login must fail saying incorrect username/password.
+        System.out.println("loginPage.errorMessage.getText() = " + loginPage.errorMessage.getText());
+        Assert.assertTrue(loginPage.errorMessage.getText().contains(" is incorrect"));
+
+    }
+}
