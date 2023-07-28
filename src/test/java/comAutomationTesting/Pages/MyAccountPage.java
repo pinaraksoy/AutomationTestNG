12a60aa9a1df0870fd368e94af79b20405aeeb79
pinaraksoy <pinaraksoy_22@hotmail.com>
2023-07-28 08:44:26 +0300

    updated

--- src/test/java/comAutomationTesting/Pages/MyAccountPage.java
+++ src/test/java/comAutomationTesting/Pages/MyAccountPage.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
-    public LoginPage() {
+public class MyAccountPage {
+    public MyAccountPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
