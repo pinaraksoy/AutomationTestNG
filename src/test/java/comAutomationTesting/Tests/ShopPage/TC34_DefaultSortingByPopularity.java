041243c54f50c2b865dac2d9db31aaf0e4dd10c8
pinaraksoy <pinaraksoy_22@hotmail.com>
2023-07-31 23:28:51 +0300

    TC34 added

---
+++ src/test/java/comAutomationTesting/Tests/ShopPage/TC34_DefaultSortingByPopularity.java
@@ -0,0 +1,31 @@
+package comAutomationTesting.Tests.ShopPage;
+
+import comAutomationTesting.Pages.ShopPage;
+import comAutomationTesting.utilities.ConfigurationReader;
+import comAutomationTesting.utilities.Driver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC34_DefaultSortingByPopularity {
+    ShopPage shopPage = new ShopPage();
+
+    @Test
+    public void defaultSortingByPopularity() {
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
+        //3) Click on Shop Menu
+        shopPage.shopButton.click();
+        Driver.getDriver().navigate().refresh();
+        shopPage.shopButton.click();
+
+        //4) Click on Sort by Popularity item in Default sorting dropdown
+        Select defaultSorting = new Select(shopPage.defaultSortingDropdown);
+        defaultSorting.selectByVisibleText("Sort by popularity");
+
+         //5) Now user can view the popular products only
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("popularity"));
+    }
+}
