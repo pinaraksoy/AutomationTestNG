6b78ec06ab92b39c36b195164a0d082a69806637
pinaraksoy <pinaraksoy_22@hotmail.com>
2023-07-31 23:13:58 +0300

    ShopPage created

---
+++ src/test/java/comAutomationTesting/Pages/ShopPage.java
@@ -0,0 +1,40 @@
+package comAutomationTesting.Pages;
+
+import comAutomationTesting.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ShopPage {
+    public ShopPage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(linkText = "Shop")
+    public WebElement shopButton;
+
+    @FindBy(css = "span[style='left: 85.7143%;']")
+    public WebElement filterByPric450;
+
+    @FindBy(css = "span[style='left: 0%;']")
+    public WebElement filterByPric150;
+
+    @FindBy(xpath = "//span[@class='ui-slider-handle ui-corner-all ui-state-default'][2]")
+    public WebElement filterRightButton;
+
+    @FindBy(xpath = "//button[.='Filter']")
+    public WebElement filterButton;
+
+  @FindBy(xpath = "//a[.='Android']")
+    public WebElement productAndroid;
+
+  @FindBy(xpath = "//nav[@class='woocommerce-breadcrumb']")
+    public WebElement homeChosenProduct;
+
+
+
+
+
+
+}
